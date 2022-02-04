9c0766afa8a81fba726c248fe1f573120ea8920f
jvcataquiz <jvcataquiz@gmail.com>
2022-02-04 23:02:53 +0800

    First i created a json file then i tried to open the json file using System io but it's not working so i install newtownsoft.json in my project, then i successfully open the json file, but it's not organize prperly. Using Json.DeserializeObject i can get the value of any variable in my jsonfile and show it in message Box. for now , im trying to pass the value and convert it to pdf

---
+++ Resume/ResumeGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Resume
+{
+    public partial class ResumeGenerator : Form
+    {
+        string _path = @"C:\Users\Jv Cataquiz\output\resume.json";
+        public ResumeGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filedata = File.ReadAllText(_path);
+            Resumepdf record = JsonConvert.DeserializeObject<Resumepdf>(filedata);
+            MessageBox.Show(record.Name);
+            MessageBox.Show(record.Contact);
+        }
+        public class Resumepdf
+        {
+            public string Name { get; set; }
+            public string Contact { get; set; }
+           
+    }
+
+  
+}
+}
